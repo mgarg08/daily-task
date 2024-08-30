5bfb0ff8ae62460f13bd210b4d255535d169081c
Mudit Garg <mudit.garg1@tothenew.com>
2024-08-30 17:42:58 +0530

    first task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+
+          <li className="dropdown">
+            <a href="#" class="dropbtn">
+              Services
+            </a>
+            <ul className="dropdown-content">
+              <li>
+                <a href="#">Web Development</a>
+              </li>
+              <li>
+                <a href="#">App Development</a>
+              </li>
+              <li>
+                <a href="#">SEO Services</a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="dropdown">
+            <a href="#" class="dropbtn">
+              Products
+            </a>
+            <ul className="dropdown-content">
+              <li>
+                <a href="#">Product 1</a>
+              </li>
+              <li>
+                <a href="#">Product 2</a>
+              </li>
+              <li>
+                <a href="#">Product 3</a>
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
