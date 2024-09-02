a450606edb13204fd94e54b9349e488cb510a7ea
Mudit Garg <mudit.garg1@tothenew.com>
2024-09-02 17:51:04 +0530

    dynamic implementation

--- src/Component/September1/index.js
+++ src/Component/September1/index.js
@@ -1,59 +1,81 @@
 import React from "react";
 import "./style.css";
 
+const cardData = [
+    {
+      title: "First Tier",
+      popular: false,
+      tier:1,
+      heading:
+        "The perfect plan for those just getting started. Free for 7 days!",
+      items: [
+        "Super super benefit!",
+        "Another awesome benefit!",
+        "The best benefit ever!",
+        "Okay, this is the best.",
+        "This one's okay",
+        "Beneffffffit!",
+        "One last benefit.",
+      ],
+      price: "$9.99/month",
+    },
+    {
+      title: "Second Tier",
+      popular: true,
+      tier:2,
+      heading:
+        "Our most popular tier and perfect for freelancers and small businesses.",
+      items: [
+        "Super super benefit!",
+        "Another awesome benefit!",
+        "The best benefit ever!",
+        "Okay, this is the best.",
+        "This one's okay",
+        "Beneffffffit!",
+        "One last benefit.",
+      ],
+      price: "$13.99/month",
+    },
+    {
+      title: "Third Tier",
+      popular: false,
+      tier:3,
+      heading:
+        "This is our POWERHOUSE! Running a big business? This is for you.",
+      items: [
+        "Super super benefit!",
+        "Another awesome benefit!",
+        "The best benefit ever!",
+        "Okay, this is the best.",
+        "This one's okay",
+        "Beneffffffit!",
+        "One last benefit.",
+      ],
+      price: "$19.99/month",
+    },
+  ];
+
 export default function September1() {
     return (
-      <div class="pricing-container">
-          <div class="pricing-card">
-              <button className="heading-button">First Tier</button>
-              <p className="heading">The perfect plan for those just getting started. Free for 7 days!</p>
-              <ul>
-                  <li>Super super benefit!</li>
-                  <li>Another awesome benefit!</li>
-                  <li>The best benefit ever!</li>
-                  <li>Okay, this is the best.</li>
-                  <li>This one's okay.</li>
-                  <li>Beneffffffit!</li>
-                  <li>One last benefit.</li>
-              </ul>
-              <div class="price">$9.99/month</div>
-              <button>Choose</button>
-          </div>
-  
-          <div class="pricing-card popular">
+        <div className="pricing-container">
+          {cardData.map((data, tier) => (
+            <div  className={`pricing-card tier${data?.tier || 1}`} key={tier}>
+            {data?.popular? 
             <span>
-            <button className="heading-button">Second Tier</button>
-            <p>Most Popular!</p>
+                <button className="heading-button">{data?.title}</button>
+                <button className="heading-mostPopular">Most Popular</button>
             </span>
-              <p className="heading">Our most popular tier and perfect for freelancers and small businesses.</p>
-              <ul>
-                  <li>Super super benefit!</li>
-                  <li>Another awesome benefit!</li>
-                  <li>The best benefit ever!</li>
-                  <li>Okay, this is the best.</li>
-                  <li>This one's okay.</li>
-                  <li>Beneffffffit!</li>
-                  <li>One last benefit.</li>
+            :<button className="heading-button ">{data?.title}</button>}  
+              <p className="pricing-heading">{data?.heading}</p>
+              <ul className="pricing-card-list">
+                {data?.items?.map((item) => (
+                  <li className="pricing-card-list-items">{item}</li>
+                ))}
               </ul>
-              <div class="price">$13.99/month</div>
-              <button>Choose</button>
-          </div>
-  
-          <div class="pricing-card">
-          <button className="heading-button">third Tier</button>
-              <p className="heading">This is our POWERHOUSE! Running a big business? This is for you.</p>
-              <ul>
-                  <li>Super super benefit!</li>
-                  <li>Another awesome benefit!</li>
-                  <li>The best benefit ever!</li>
-                  <li>Okay, this is the best.</li>
-                  <li>This one's okay.</li>
-                  <li>Beneffffffit!</li>
-                  <li>One last benefit.</li>
-              </ul>
-              <div class="price">$19.99/month</div>
-              <button>Choose</button>
-          </div>
-      </div>
-    );
-  }
+              <div className="price">{data?.price}</div>
+              <button className="choose-button">Choose</button>
+            </div>
+          ))}
+        </div>
+      );
+}
